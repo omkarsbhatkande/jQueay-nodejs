9158601e2fda258a825b4b8d592a98c7bffe3db9
omkar <omkarsbhatkande@gmail.com>
2024-10-21 18:21:31 +0530

    added tailwing setup

--- nodejs/projects/airbnb/app.js
+++ nodejs/projects/airbnb/app.js
@@ -13,6 +13,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(userRouter);
 app.use("/host",hostRouter);
 
+app.use(express.static(path.join(rootDir,"public")))
+
 app.use((req,res,next)=>{
   //res.status(404);
   res.sendFile(path.join(rootDir,"views","/404.html")) 
