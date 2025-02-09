06afee46e631223f180177d24378b6da6eded477
omkar <omkarsbhatkande@gmail.com>
2025-02-09 10:49:45 +0530

    gettig error

--- nodejs/projects/jobPortal/middlewares/errorMiddleware.js
+++ nodejs/projects/jobPortal/middlewares/errorMiddleware.js
@@ -1,9 +1,9 @@
 //error middle ware // NEXT Function
 const errorMiddleware = (req, res, next) => {
-  console.log(err);
+  console.log(error);
   const defaultErrors = {
     statusCode: 500,
-    message: err,
+    message: error,
   };
 
   // res.status(500).send({
@@ -13,18 +13,18 @@ const errorMiddleware = (req, res, next) => {
   // });
 
   //missing filed error
-  if (err.name === "validationError") {
+  if (error.name === "validationError") {
     defaultErrors.statusCode = 400;
-    defaultErrors.message = Object.values(err.errors)
+    defaultErrors.message = Object.values(error.errors)
       .map((item) => item.message)
       .join(",");
   }
 
   //duplicate error
-  if (err.code && err.code === 11000) {
+  if (error.code && error.code === 11000) {
     defaultErrors.statusCode = 400;
     defaultErrors.message = `${Object.keys(
-      err.keyValue
+      error.keyValue
     )} field has to be unique`;
   }
   res.status(defaultErrors.statusCode).json({ message: defaultErrors.message });
